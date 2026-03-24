6664ccb77d17dce06c599f022f3dd227d0b6bcbe
gtan <g.tan@arcanys.com>
2026-03-24 16:00:16 +0800

    added auto scroll on navigate

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import SiteFooter from "./SiteFooter";
 import NavBar from "./NavBar";
 
-const Layout = () => (
-  <div className="min-h-screen flex flex-col transition-colors duration-500 selection:bg-purple-600 selection:text-white overflow-x-hidden">
-    <NavBar />
-    <div className="flex-1">
-      <Outlet />
+const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return (
+    <div className="min-h-screen flex flex-col transition-colors duration-500 selection:bg-purple-600 selection:text-white overflow-x-hidden">
+      <NavBar />
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <SiteFooter />
     </div>
-    <SiteFooter />
-  </div>
-);
+  );
+};
 
 export default Layout;
